0073bd4c6e60e13bf5a30ed793e15edb41b7dc75
Sameera Madushan Silva <madushansameera499@gmail.com>
2024-10-27 20:13:01 +0530

    first commit

---
+++ src/test/java/com/example/CropMonitoringSystem/CropMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CropMonitoringSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
